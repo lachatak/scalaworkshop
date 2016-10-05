7ae461e484f2f2ac6ce9445692441b24be24f01d
Tomasz Rosiek <trosiek@gumtree.com>
2016-10-05 15:21:05 +0100

    Either & Disjunctions - Scalaz

--- scalaz/src/main/scala/org/kaloz/gumtreeworkshop/scalaz/DisjunctionsAndValidations.scala
+++ scalaz/src/main/scala/org/kaloz/gumtreeworkshop/scalaz/DisjunctionsAndValidations.scala
@@ -1,31 +1,44 @@
 package org.kaloz.gumtreeworkshop.scalaz
 
+import scalaz._
+import Scalaz._
 /* Standard Scala provides special type that can be used to handle (successful or not) result of some operation.
    It's called Either[L, R], that has two possible subtypes - Left(value : L) or Right(value : R).
 
    Either can have either right value (by convention it's successful result of the operation), or the left value
    (by convention - the unsuccessful result of the operation).
 
+   ScalaZ introduces Disjunction - alternative version of Either[L,P] which is a monad, so that we can use for
+   comprehension.
+
+   Disjunction comes with lots of utility implicit conversions and functions that make the code simplier
+
   * */
 object DisjunctionsAndValidations extends App {
 
-  def div(x : Int, y : Int): Either[String, Int] = if (y == 0) Left("Divide by 0") else Right(x / y)
+  def div(x : Int, y : Int): Disjunction[String, Int] =
+    if (y == 0) "Divide by 0".left
+    else (x / y).right
 
   println(div(7, 5))
   println(div(7, 0))
 
-
   //You can use pattern matching to perform different operations depending on the result
 
   div(7, 5) match {
-    case Right(value) => println(s"Success $value")
-    case Left(error) => println(s"Error $error")
+    case \/-(value) => println(s"Success $value")
+    case -\/(error) => println(s"Error $error")
   }
 
-  //What is important, contrary to Option, the Either is not a monad, and we cannot use it in for
-  //comprehensions.
+  //One of the advantages of Disjunction over Either is that we cane use it in for comprehensions
 
+  val result = for (
+    x <- div(10, 2);
+    y <- div(9, 3)
+  ) yield x + y
 
+  println(result)
+
+  //EXERCISE: Check what will be the result of for comprehension when the first div fails, what if the second div fails
 
 }
-
